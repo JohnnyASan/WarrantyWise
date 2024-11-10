b3a783ac6cd4c9c4645026abfea9c0765d9203b2
JohnnyASan <johnanthonysanabria@gmail.com>
2024-11-09 18:32:43 -0700

    added trust proxy line

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ require('dotenv').config();
 
 const port = 3000;
 const session = require('express-session'); 
+
+app.set('trust proxy', 1); // trust first proxy
 app.use(session({ 
   secret: process.env.SESSION_SECRET_KEY, 
   resave: false, 
