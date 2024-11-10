a20b704a5fdc6ad4227bd279a85a0175e2678aae
JohnnyASan <johnanthonysanabria@gmail.com>
2024-11-09 20:44:59 -0700

    updated routing to dashboard

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -3,6 +3,7 @@ const mongodb = require('../utils/mongodb');
 var ObjectId = require('mongodb').ObjectId;
 const path = require('path');
 const axios = require('axios');
+const userController = require('./user');
 
 const getIndex = async (req, res) => {
   res.sendFile(path.join(__dirname, '../static/index.html'));
@@ -24,11 +25,12 @@ const oAuthCallback = async ({ query: { code } }, res) => {
   axios
     .post('https://github.com/login/oauth/access_token', body, opts)
     .then((_res) => _res.data.access_token)
-    .then((token) => {
+    .then(async (token) => {
       // eslint-disable-next-line no-console
       console.log('My token:', token);
+      var userUpsertResponse = await userController.createOrUpdate({ params: { access_token: token}});
 
-      res.redirect(`/dashboard?token=${token}`);
+      res.redirect(`/dashboard?userId=${userUpsertResponse.githubId}`);
     })
     .catch((err) => res.status(500).json({ err: err.message }));
 };
---
+++ src/controllers/user.js
@@ -0,0 +1,55 @@
+const mongodb = require('../utils/mongodb');
+var ObjectId = require('mongodb').ObjectId;
+const axios = require('axios');
+
+const getById = async (req, res) => {
+    try {
+        const userId = new ObjectId(req.params.id);
+        const result = await mongodb.getDb().db('warrantywise').collection('users').find({ _id: userId });
+        result.toArray().then((lists) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(lists[0]);
+        });
+
+    }
+    catch {
+        throw Error('Something happened while trying to get user by Id');
+    }
+};
+
+
+const createOrUpdate = async (req, res) => {
+    try {
+        const access_token = req.params.access_token;
+        const opts = { headers: { accept: 'application/json', authorization: `Bearer ${access_token}` } };
+
+        axios.get('https://api.github.com/user', opts)
+        .then((_res) => { _res.login, _res.avatar_url, _res.id})
+        .then(async (userDetails) => {
+            const user = {
+                username: userDetails.login,
+                profileImage: userDetails.avatar_url,
+                githubId: userDetails.id,
+                githubToken: access_token
+            }
+            var response = null;
+            const existingUser = await mongodb.get().db('warrantywise').collection('users').findOne({ githubId: userDetails.id});
+            if (existingUser) {
+                response = await mongodb.getDb().db('warrantywise').collection('users').replaceOne({ githubId: userDetails.id }, user);
+            } else {
+                response = await mongodb.getDb().db('warrantywise').collection('users').insertOne(user);
+            } 
+        
+            res.status(201).json(response);
+        });    
+    }
+    catch {
+     throw Error(response.error || 'Some error occurred while creating the user.');
+    }
+     
+ };
+
+module.exports = {
+    getById,
+    createOrUpdate
+}
